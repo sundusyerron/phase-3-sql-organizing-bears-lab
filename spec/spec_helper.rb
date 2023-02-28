1c70ee035acde0baf27ac00d298aaa5fa46c8907
Sundus <sundusaden105@gmail.com>
2023-02-28 15:20:29 +0300

    completed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -75,7 +75,7 @@ RSpec.configure do |config|
     # For more details, see:
     #   - http://teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
     mocks.syntax = :expect
-
+    
     # Prevents you from mocking or stubbing a method that does not exist on
     # a real object. This is generally recommended.
     mocks.verify_partial_doubles = true
